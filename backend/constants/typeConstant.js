24c747e5010f5dc346760cc627d9918b55b6edda
Kuldeep <c0dysharma01@gmail.com>
2022-12-25 21:44:20 +0530

    Added model validations

--- backend/constants/typeConstant.js
+++ backend/constants/typeConstant.js
@@ -1,3 +1,5 @@
 exports.messageType = Object.freeze({
   email: 'email',
+  sms: 'sms',
+  whatsapp: 'whatsapp',
 });
--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const typeConstant = require('../constants/typeConstant');
 
 const eventSchema = mongoose.Schema(
@@ -6,23 +7,41 @@ const eventSchema = mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
+      minLength: [4, 'Event name string length should be >= 4'],
+      maxLength: [15, 'Event name string length should be < 25'],
     },
     to: {
       type: String,
       required: true,
+      trim: true,
+      validate: {
+        validator: validator.isEmail,
+        message: 'Enter a valid receipent email address.',
+      },
     },
     from: {
       type: String,
       required: true,
+      trim: true,
+      validate: {
+        validator: validator.isEmail,
+        message: 'Enter a valid from email address.',
+      },
     },
     type: {
       type: String,
       required: true,
+      trim: true,
       default: typeConstant.messageType.email,
+      enum: Object.values(typeConstant.messageType),
     },
     messageBody: {
       type: String,
+      trim: true,
       required: true,
+      minLength: [4, 'Message body string length should be >= 4'],
+      maxLength: [100, 'Message body string length should be < 100'],
     },
     dateTime: {
       type: Date,
@@ -31,7 +50,10 @@ const eventSchema = mongoose.Schema(
     },
     messageTitle: {
       type: String,
+      trim: true,
       required: true,
+      minLength: [4, 'Message body string length should be >= 4'],
+      maxLength: [50, 'Message body string length should be < 50'],
     },
     isActive: {
       type: Boolean,
